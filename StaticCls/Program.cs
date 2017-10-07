4a7a1ea631693bfbd05d91e8df39dec26d5c3a7d
Sumedh Saraf <saraf.sumedh@hotmail.com>
2017-10-07 18:21:27 -0500

    Add files via upload

---
+++ StaticCls/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaticCls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestStat t = new TestStat();        //not allowed obj of static class.
+            TestStat.show();
+
+            NewC obj = new NewC();
+            NewC obj1 = new NewC();
+            NewC obj2 = new NewC();
+
+            Console.WriteLine("cnt: {0}",NewC.cnt);
+            Console.ReadLine();
+        }
+    }
+
+        public static class TestStat
+        {
+            public static int i = 50;
+            public static void show()
+            {
+                Console.WriteLine("\n Static Method!");
+            }
+        }
+
+        public class NewC
+        {
+            public static int cnt = 0;          //non-static class can contain static members.
+            public NewC()
+            {
+                cnt++;
+                Console.WriteLine("Default ctor called");
+            }
+            static NewC()
+            {
+                Console.WriteLine("Static ctor Called");
+            }
+        }
+}
---
+++ testdll/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cmath;
+
+
+namespace testdllpri
+{
+    class Program:Cmath
+    {
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            int r1=p1.pbadd(10, 20);
+            int r2 = p1.pimod(9, 2);
+            int r3 = p1.promul(2, 3);
+            Console.WriteLine("pbadd: {0}, pimod: {1}, promul: {2}",r1,r2,r3);
+            Console.ReadLine();
+        }
+    }
+}
